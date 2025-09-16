ac1f0fa993a47ea063e08e90f8031484233478a7
jpnacrd <jonacrd@gmail.com>
2025-09-15 22:42:49 -0300

    feat: add Prettier configuration with Astro support

---
+++ prettier.config.js
@@ -0,0 +1,38 @@
+/** @type {import("prettier").Config} */
+export default {
+  // Configuración general
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+
+  // Configuraciones específicas por tipo de archivo
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+    {
+      files: '*.md',
+      options: {
+        printWidth: 100,
+        proseWrap: 'always',
+      },
+    },
+    {
+      files: '*.json',
+      options: {
+        printWidth: 120,
+      },
+    },
+  ],
+};
